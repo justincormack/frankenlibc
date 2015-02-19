d1fb713c470ee34997f6eac0b127b9f091af6d71
Justin Cormack <justin@specialbusservice.com>
2015-02-19 23:48:22 +0000

    build fixes

--- buildc.sh
+++ buildc.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-set -e
-
 # nothing else supported yet
 OS=linux
 ARCH=x86_64
@@ -12,32 +10,36 @@ RUMPSRC=rumpsrc
 ( [ ! -f rumpsrc/build.sh ] && git submodule update --init rumpsrc \
 	&& cd rumpsrc && cat ../rumpuser.patch | patch -p1 )
 
+set -e
+
 . ./buildrump.sh/subr.sh
+
+# We should not have to undefine Linux, wrapper already should XXX
 
 ./buildrump.sh/buildrump.sh \
 	-V RUMP_CURLWP=hypercall -V MKPIC=no -V RUMP_KERNEL_IS_LIBC=1 \
-	-F CFLAGS=-fno-stack-protector -k -s ${RUMPSRC} \
+	-F CFLAGS=-fno-stack-protector -F CPPFLAGS=-nostdinc \
+	-F CPPFLAGS='-Ulinux -U__linux -U__linux__ -U__gnu_linux__ -D__NetBSD__' \
+	-k -s ${RUMPSRC} \
 	tools build kernelheaders install
 
 RUMPMAKE=${PWD}/obj/tooldir/rumpmake
+
+# -k build does not install rumpuser headers
+mkdir -p rump/include/rump
+cp ${RUMPSRC}/lib/librumpuser/rumpuser_component.h rump/include/rump
 
 ( cd libc && make )
 
 ar cr lib/libc.a obj/libc/*.o
 
-# -k build does not install rumpuser headers 
-mkdir -p rump/include/rump
-cp src/lib/librumpuser/rumpuser_component.h rump/include/rump
-
 mkdir -p obj/lib/librumpuser
 
-#export CPPFLAGS="-DRUMPUSER_CONFIG=yes -nostdinc -I${PWD}/include -I${PWD}/rump/include -I${PWD}/src/sys/rump/include"
-
 # We don't yet have enough to be able to run configure, coming soon
-cp rumpuser_config.h src/lib/librumpuser/
+cp rumpuser_config.h ${RUMPSRC}/lib/librumpuser/
 
-( CPPFLAGS="-DRUMPUSER_CONFIG=yes -nostdinc -I${PWD}/include -I${PWD}/rump/include -I${PWD}/src/sys/rump/include" \
-	cd ${RUMPSRC}/lib/librumpuser && ${RUMPMAKE} RUMPUSER_THREADS=fiber )
+( export CPPFLAGS="-DRUMPUSER_CONFIG=yes -nostdinc -I${PWD}/include -I${PWD}/rump/include -I${PWD}/src/sys/rump/include" \
+	&& cd ${RUMPSRC}/lib/librumpuser && ${RUMPMAKE} RUMPUSER_THREADS=fiber )
 
 LIBS="$(stdlibs ${RUMPSRC})"
 if [ "$(${RUMPMAKE} -f rumptools/mk.conf -V '${_BUILDRUMP_CXX}')" = 'yes' ]
---
+++ rumpuser_config.h
@@ -0,0 +1,20 @@
+#define HAVE_ALIGNED_ALLOC 1
+#define HAVE_CLOCKID_T 1
+#define HAVE_CLOCK_GETTIME 1
+#define HAVE_CLOCK_NANOSLEEP 1
+#define HAVE_GETRANDOM 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_REGISTER_T 1
+#define HAVE_STDINT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRING_H 1
+#define HAVE_SYS_CDEFS_H 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_UNISTD_H 1
+#define PACKAGE_BUGREPORT "http://rumpkernel.org/"
+#define PACKAGE_NAME "rumpuser-posix"
+#define PACKAGE_STRING "rumpuser-posix 999"
+#define PACKAGE_TARNAME "rumpuser-posix"
+#define PACKAGE_URL ""
+#define PACKAGE_VERSION "999"
+#define STDC_HEADERS 1
