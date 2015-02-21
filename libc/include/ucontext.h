04a1ef31f5055caeaf916bf9e5d7c0a687015409
Justin Cormack <justin@specialbusservice.com>
2015-02-21 11:29:35 +0000

    detect arch in includes

--- libc/include/ucontext.h
+++ libc/include/ucontext.h
@@ -1,1 +1,15 @@
-#include <arch/ucontext.h>
+#if defined(__x86_64__)
+#include <x86_64/ucontext.h>
+#elif defined(__i386__)
+#include <i386/ucontext.h>
+#elif defined(__ARMEL__) || defined(__ARMEB__)
+#include <arm/ucontext.h>
+#elif defined(__AARCH64EL__) || defined (__AARCH64EB__)
+#include <aarch64/ucontext.h>
+#elif defined(__PPC64__)
+#include <ppc64/ucontext.h>
+#elif defined(__PPC__)
+#include <ppc/ucontext.h>
+#else
+#error "Unknown architecture"
+#endif
