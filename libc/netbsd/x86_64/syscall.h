0cafe2c5f407b5f97e043426019d4bc28e6d5283
Justin Cormack <justin@specialbusservice.com>
2015-02-24 10:27:42 +0000

    netbsd mmap syscall needs special handling

--- libc/netbsd/x86_64/syscall.h
+++ libc/netbsd/x86_64/syscall.h
@@ -10,3 +10,17 @@ name:; \
 	mov	$-1, %rax; \
 _syscall_return:; \
 	ret;
+
+#define SYSCALL_MMAP(sc, name) \
+.global name; \
+.type name,@function; \
+name:; \
+	mov	$0, %r9; \
+	mov	$SYS_mmap, %rax; \
+	mov	%rcx, %r10; \
+	syscall; \
+	jnc	_syscall_return; \
+	mov	%rax, errno; \
+	mov	$-1, %rax; \
+_syscall_return:; \
+	ret
