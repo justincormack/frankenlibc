d343e39d5f7f22fb76626c7f6619f66baac0abab
Justin Cormack <justin@specialbusservice.com>
2015-02-25 09:47:12 +0000

    sigh, make it work on compilers that declare explicit signed

--- libc/include/stdint.h
+++ libc/include/stdint.h
@@ -1,15 +1,31 @@
 #ifndef _STDINT_H_
 #define _STDINT_H_
 
+#ifndef __UINT8_TYPE__
+#define __UINT8_TYPE__ unsigned __INT8_TYPE__
+#endif
+#ifndef __UINT16_TYPE__
+#define __UINT16_TYPE__ unsigned __INT16_TYPE__
+#endif
+#ifndef __UINT32_TYPE__
+#define __UINT32_TYPE__ unsigned __INT32_TYPE__
+#endif
+#ifndef __UINT32_TYPE__
+#define __UINT32_TYPE__ unsigned __INT32_TYPE__
+#endif
+#ifndef __UINTPTR_TYPE__
+#define __UINTPTR_TYPE__ unsigned __INTPTR_TYPE__
+#endif
+
 typedef __INT8_TYPE__ int8_t;
-typedef unsigned __INT8_TYPE__ uint8_t;
+typedef __UINT8_TYPE__ uint8_t;
 typedef __INT16_TYPE__ int16_t;
-typedef unsigned __INT16_TYPE__ uint16_t;
+typedef __UINT16_TYPE__ uint16_t;
 typedef __INT32_TYPE__ int32_t;
-typedef unsigned __INT32_TYPE__ uint32_t;
+typedef __UINT32_TYPE__ uint32_t;
 typedef __INT64_TYPE__ int64_t;
-typedef unsigned __INT64_TYPE__ uint64_t;
+typedef __UINT64_TYPE__ uint64_t;
 typedef __INTPTR_TYPE__ intptr_t;
-typedef unsigned __INTPTR_TYPE__ uintptr_t;
+typedef __UINTPTR_TYPE__ uintptr_t;
 
 #endif
