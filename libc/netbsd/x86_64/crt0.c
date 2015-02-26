6e63eaccac657664cf7ae337560ad4eb93e0c412
Justin Cormack <justin@specialbusservice.com>
2015-02-26 15:57:59 +0000

    fix netbsd initialisation

--- libc/netbsd/x86_64/crt0.c
+++ libc/netbsd/x86_64/crt0.c
@@ -1,13 +1,10 @@
 __asm__ (".text\n\t"
 	 ".global _start\n\t"
 	 "_start:\n\t"
-	 "	pop	%rsi\n\t"		/* argc */
-	 "	mov	%rsp, %rdx\n\t"		/* argv */
-	 "	push	%rsi\n\t"
-	 "	mov	%rdi, %rcx\n\t"		/* envp */
-	 "	shl	$3, %rcx\n\t"
-	 "	add	$8, %rcx\n\t"
-	 "	add	%rdx, %rcx\n\t"
-	 "	mov	$main,%rdi\n\t"		/* 1st arg: application entry ip */
-	 "	call	__libc_start_main\n\t"
+	 "andq	$~15,%rsp\n\t"
+	 "subq	$8,%rsp\n\t"
+	 "movq	%rdx, %rdi\n\t"
+	 "movq	%rcx, %rsi\n\t"
+	 "movq	%rbx, %rdx\n\t"
+	 "	call	__platform_init\n\t"
 	 "1:	jmp 1b\n\t");
