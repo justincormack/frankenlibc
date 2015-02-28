d6b315516fac8eb3cf543f8f2f1540c3aa6bc5e5
Justin Cormack <justin@specialbusservice.com>
2015-02-28 14:02:47 +0000

    const it to stop it using tls, then at least we can test scheduling

--- test/hello.c
+++ test/hello.c
@@ -4,11 +4,11 @@
 int
 main()
 {
-	//static struct timespec ts = {0, 1000};
+	static const struct timespec ts = {0, 1000};
 
-	//nanosleep(&ts, NULL);
+	nanosleep(&ts, NULL);
 	fprintf(stderr, "I beheld the wretch — the miserable monster whom I had created.\n");
-	//nanosleep(&ts, NULL);
+	nanosleep(&ts, NULL);
 
 	return 0;
 }
