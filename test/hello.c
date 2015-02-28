7486eae44d32e80833b557dd30c810812729d474
Justin Cormack <justin@specialbusservice.com>
2015-02-28 12:30:28 +0000

    sleep in test to exercise schuling

--- test/hello.c
+++ test/hello.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+#include <time.h>
 
 int
 main()
 {
+	struct timespec ts = {0, 1000};
 
+	nanosleep(&ts, NULL);
 	fprintf(stderr, "I beheld the wretch — the miserable monster whom I had created.\n");
+	nanosleep(&ts, NULL);
 
 	return 0;
 }
