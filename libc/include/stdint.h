224ae022dabc7d27b9fbbfd0722ed747c4919c1a
Justin Cormack <justin@specialbusservice.com>
2015-03-01 16:08:44 +0000

    more options in stdint for older compilers

--- libc/include/stdint.h
+++ libc/include/stdint.h
@@ -2,15 +2,31 @@
 #define _STDINT_H_
 
 #ifndef __UINT8_TYPE__
+#ifndef __INT8_TYPE__
+#define __INT8_TYPE__ char
+#endif
 #define __UINT8_TYPE__ unsigned __INT8_TYPE__
+#endif
+#ifndef __INT16_TYPE__
+#define __INT16_TYPE__ short
 #endif
 #ifndef __UINT16_TYPE__
 #define __UINT16_TYPE__ unsigned __INT16_TYPE__
 #endif
 #ifndef __UINT32_TYPE__
+#ifndef __INT32_TYPE__
+#define __INT32_TYPE__ int
+#endif
 #define __UINT32_TYPE__ unsigned __INT32_TYPE__
 #endif
 #ifndef __UINT64_TYPE__
+#ifndef __INT64_TYPE_
+#if __SIZEOF_LONG__ == 4
+#define __INT64_TYPE__ long long
+#else
+#define __INT64_TYPE__ long
+#endif
+#endif
 #define __UINT64_TYPE__ unsigned __INT64_TYPE__
 #endif
 #ifndef __UINTPTR_TYPE__
