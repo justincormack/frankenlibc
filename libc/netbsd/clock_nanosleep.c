2c177a025c6143e5cd0e7d3bcd1161f449b7ec60
Justin Cormack <justin@specialbusservice.com>
2015-03-01 22:40:27 +0000

    test netbsd version for fallbacks

--- libc/netbsd/clock_nanosleep.c
+++ libc/netbsd/clock_nanosleep.c
@@ -1,16 +1,35 @@
 #include <errno.h>
+#include <signal.h>
 #include <time.h>
+#include <unistd.h>
+
+#define CTL_KERN 1
+#define KERN_OSREV 3
+
+int __platform_sysctl(const int *, unsigned int, void *, size_t *, const void *, size_t);
 
 int __clock_nanosleep(clockid_t, int, const struct timespec *, struct timespec *);
 int __platform_nanosleep(const struct timespec *, struct timespec *);
 
-static int __have_cns = 1;
-
 int
 clock_nanosleep(clockid_t clockid, int flags, const struct timespec *req, struct timespec *rem)
 {
+	static int __have_cns = -1;
+	const int ctl[2] = {CTL_KERN, KERN_OSREV};
+	int version;
+	size_t lenp = sizeof(int);
 	int ret;
 
+	if (__have_cns == -1) {
+		ret = __platform_sysctl(ctl, 2, &version, &lenp, NULL, 0);
+		if (ret == -1)
+			kill(0, SIGABRT);
+		/* clock_nanosleep introduced in NetBSD 7 */
+		if (version / 100000000 >= 7)
+			__have_cns = 1;
+		else
+			__have_cns = 0;
+	}
 	if (__have_cns) {
 		ret = __clock_nanosleep(clockid, flags, req, rem);
 		if (ret != -1 || errno != ENOSYS) {
@@ -19,5 +38,5 @@ clock_nanosleep(clockid_t clockid, int flags, const struct timespec *req, struct
 		__have_cns = 0;
 	}
 
-	return __platform_nanosleep(req, rem); /* NetBSD 6 has no clock_nanosleep */
+	return __platform_nanosleep(req, rem);
 }
