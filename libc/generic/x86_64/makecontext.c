f053718e85cdde06ccc74114578985a12ea793bc
Justin Cormack <justin@specialbusservice.com>
2015-03-02 00:44:33 +0000

    improve x86_64 stack alignment

--- libc/generic/x86_64/makecontext.c
+++ libc/generic/x86_64/makecontext.c
@@ -10,7 +10,7 @@ makecontext(ucontext_t *ucp, void (*fn)(void), int argc, void *arg)
 	if (argc != 1) abort();
 
 	/* need to push stack pointer, f as ip and data as first arg */
-	ucp->uc_regs[6] = stack + stack_size; /* grows down */
+	ucp->uc_regs[6] = stack + stack_size - 8; /* grows down */
 	ucp->uc_regs[7] = (unsigned long) fn;
 	ucp->uc_regs[8] = (unsigned long) arg;
 }
