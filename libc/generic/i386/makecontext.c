acaf8720c8711bdd77d38d5f1dd5d9c0286ffef6
Justin Cormack <justin@specialbusservice.com>
2015-03-02 00:52:42 +0000

    i386 secondary stack alignment

--- libc/generic/i386/makecontext.c
+++ libc/generic/i386/makecontext.c
@@ -6,12 +6,12 @@ makecontext(ucontext_t *ucp, void (*fn)(void), int argc, void *arg)
 {
 	unsigned long stack = (unsigned long) ucp->uc_stack.ss_sp;
 	unsigned long stack_size = ucp->uc_stack.ss_size;
-	unsigned long *stacktop = (unsigned long *) (stack + stack_size - 4);
+	unsigned long *stacktop = (unsigned long *) (stack + stack_size - 12);
 
 	if (argc != 1) abort();
 
 	/* need to push stack pointer, f as ip and data as first arg */
-	ucp->uc_regs[4] = stack + stack_size - 8; /* grows down */
+	ucp->uc_regs[4] = stack + stack_size - 16; /* grows down */
 	ucp->uc_regs[5] = (unsigned long) fn;
 	stacktop[0] = (unsigned long) arg;
 }
