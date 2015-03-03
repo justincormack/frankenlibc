93e02b1bf08e6fa939be6deb75e65289c196d7bb
Justin Cormack <justin@specialbusservice.com>
2015-03-03 23:10:29 +0000

    add linux fstat

---
+++ libc/linux/fstat.c
@@ -0,0 +1,30 @@
+#include <sys/stat.h>
+#include <errno.h>
+
+#include "linux.h"
+
+int __fstat(int, struct linux_stat *);
+
+int
+fstat(int fd, struct stat *st)
+{
+	int ret;
+	struct linux_stat lst;
+
+	ret = __fstat(fd, &lst);
+	if (ret == -1) {
+		errno = EBADF;
+		return -1;
+	}
+
+	st->st_size = lst.st_size;
+	st->st_mode = (LINUX_S_ISDIR (lst.st_mode) ? S_IFDIR  : 0) |
+		      (LINUX_S_ISCHR (lst.st_mode) ? S_IFCHR  : 0) |
+		      (LINUX_S_ISBLK (lst.st_mode) ? S_IFBLK  : 0) |
+		      (LINUX_S_ISREG (lst.st_mode) ? S_IFREG  : 0) |
+		      (LINUX_S_ISFIFO(lst.st_mode) ? S_IFIFO  : 0) |
+		      (LINUX_S_ISLNK (lst.st_mode) ? S_IFLNK  : 0) |
+		      (LINUX_S_ISSOCK(lst.st_mode) ? S_IFSOCK : 0);
+
+	return 0;
+}
