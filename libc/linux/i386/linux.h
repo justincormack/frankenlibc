6f0ff6874881e4ac4c305eab3db06e3d82c8bffe
Justin Cormack <justin@specialbusservice.com>
2015-03-03 22:21:53 +0000

    linux struct stat

--- libc/linux/i386/linux.h
+++ libc/linux/i386/linux.h
@@ -2,3 +2,25 @@
 
 #define ARCH_SET_FS 0x1002
 #define TCGETS 0x5401
+
+struct stat {
+	unsigned long long	st_dev;
+	unsigned char		__pad0[4];
+	unsigned long		__st_ino;
+	unsigned int		st_mode;
+	unsigned int		st_nlink;
+	unsigned long		st_uid;
+	unsigned long		st_gid;
+	unsigned long long	st_rdev;
+	unsigned char		__pad3[4];
+	long long		st_size;
+	unsigned long		st_blksize;
+	unsigned long long	st_blocks;
+	unsigned long		st_atime;
+	unsigned long		st_atime_nsec;
+	unsigned long		st_mtime;
+	unsigned int		st_mtime_nsec;
+	unsigned long		st_ctime;
+	unsigned long		st_ctime_nsec;
+	unsigned long long	st_ino;
+};
--- libc/linux/x86_64/linux.h
+++ libc/linux/x86_64/linux.h
@@ -2,3 +2,24 @@
 
 #define ARCH_SET_FS 0x1002
 #define TCGETS 0x5401
+
+struct stat {
+	unsigned long	st_dev;
+	unsigned long	st_ino;
+	unsigned long	st_nlink;
+	unsigned int	st_mode;
+	unsigned int	st_uid;
+	unsigned int	st_gid;
+	unsigned int	__pad0;
+	unsigned long	st_rdev;
+	long		st_size;
+	long		st_blksize;
+	long		st_blocks;
+	unsigned long	st_atime;
+	unsigned long	st_atime_nsec;
+	unsigned long	st_mtime;
+	unsigned long	st_mtime_nsec;
+	unsigned long	st_ctime;
+	unsigned long	st_ctime_nsec;
+	long		__unused[3];
+};
