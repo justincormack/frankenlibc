e3c812efac1c13f08a0f7965533a12bb714f10ac
Justin Cormack <justin@specialbusservice.com>
2015-03-04 13:37:52 +0000

    cleanup weak

--- libc/crt/__libc_start_main.c
+++ libc/crt/__libc_start_main.c
@@ -17,15 +17,15 @@ int errno __attribute__((weak));
 static char empty_string[] = "";
 char *__progname = empty_string;
 
-void _libc_init(void) __attribute__((weak));
-void _libc_init() {}
+void _libc_init(void);
+void _libc_init() {} __attribute__((weak)) 
 
 int __libc_start_main(int (*)(int,char **,char **), int, char **, char **);
 
-void _init(void) __attribute__ ((weak));
-void _init() {}
-void _fini(void) __attribute__ ((weak));
-void _fini() {}
+void _init(void);
+void _init() {} __attribute__ ((weak))
+void _fini(void);
+void _fini() {} __attribute__ ((weak))
 
 extern void (*const __init_array_start)() __attribute__((weak));
 extern void (*const __init_array_end)() __attribute__((weak));
@@ -36,6 +36,7 @@ void _exit(int) __attribute__ ((noreturn));
 
 /* XXX if running NetBSD libc, finalizers should be set via atexit */
 void exit(int) __attribute__ ((noreturn)) __attribute__((weak));
+
 void
 exit(int v)
 {
