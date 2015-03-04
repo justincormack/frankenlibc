1f967b9be8e293ad00a56555376ccdbeb2b4aee6
Justin Cormack <justin@specialbusservice.com>
2015-03-04 00:06:11 +0000

    freebsd struct stat

--- libc/freebsd/freebsd.h
+++ libc/freebsd/freebsd.h
@@ -1,3 +1,6 @@
+#include <stdint.h>
+#include <sys/types.h>
+
 #define CTL_KERN 1
 #define CTL_HW 6
 
@@ -44,3 +47,52 @@ struct termios {
 #define _IOR(g,n,t)	_IOC(IOC_OUT, (g), (n), sizeof(t))
 
 #define TIOCGETA _IOR('t', 19, struct termios)
+
+#define FREEBSD_S_IFMT   0170000
+
+#define FREEBSD_S_IFIFO  0010000
+#define FREEBSD_S_IFCHR  0020000
+#define FREEBSD_S_IFDIR  0040000
+#define FREEBSD_S_IFBLK  0060000
+#define FREEBSD_S_IFREG  0100000
+#define FREEBSD_S_IFLNK  0120000
+#define FREEBSD_S_IFSOCK 0140000
+
+#define FREEBSD_S_ISDIR(mode)  (((mode) & FREEBSD_S_IFMT) == FREEBSD_S_IFDIR)
+#define FREEBSD_S_ISCHR(mode)  (((mode) & FREEBSD_S_IFMT) == FREEBSD_S_IFCHR)
+#define FREEBSD_S_ISBLK(mode)  (((mode) & FREEBSD_S_IFMT) == FREEBSD_S_IFBLK)
+#define FREEBSD_S_ISREG(mode)  (((mode) & FREEBSD_S_IFMT) == FREEBSD_S_IFREG)
+#define FREEBSD_S_ISFIFO(mode) (((mode) & FREEBSD_S_IFMT) == FREEBSD_S_IFIFO)
+#define FREEBSD_S_ISLNK(mode)  (((mode) & FREEBSD_S_IFMT) == FREEBSD_S_IFLNK)
+#define FREEBSD_S_ISSOCK(mode) (((mode) & FREEBSD_S_IFMT) == FREEBSD_S_IFSOCK)
+
+typedef uint32_t freebsd_dev_t;
+typedef uint32_t freebsd_ino_t;
+typedef uint16_t freebsd_mode_t;
+typedef uint16_t freebsd_nlink_t;
+typedef int64_t  freebsd_blkcnt_t;
+typedef uint32_t freebsd_blksize_t;
+typedef uint32_t freebsd_fflags_t;
+
+struct freebsd_stat {
+  freebsd_dev_t			st_dev;
+  freebsd_ino_t			st_ino;
+  freebsd_mode_t		st_mode;
+  freebsd_nlink_t		st_nlink;
+  uid_t				st_uid;
+  gid_t				st_gid;
+  freebsd_dev_t			st_rdev;
+  struct freebsd_timespec	st_atim;
+  struct freebsd_timespec	st_mtim;
+  struct freebsd_timespec	st_ctim;
+  off_t				st_size;
+  freebsd_blkcnt_t		st_blocks;
+  freebsd_blksize_t		st_blksize;
+  freebsd_fflags_t		st_flags;
+  uint32_t			st_gen;
+  int32_t			st_lspare;
+  struct freebsd_timespec	st_birthtim;
+  unsigned int :(8 / 2) * (16 - (int)sizeof(struct freebsd_timespec));
+  unsigned int :(8 / 2) * (16 - (int)sizeof(struct freebsd_timespec));
+};
+
