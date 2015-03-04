2b22ce335bbf900d80b16306fd6accae529c8654
Justin Cormack <justin@specialbusservice.com>
2015-03-04 10:10:54 +0000

    cleanup rumpuser_port

--- librumpuser/rumpuser_port.h
+++ librumpuser/rumpuser_port.h
@@ -3,15 +3,6 @@
 #ifndef _LIB_LIBRUMPUSER_RUMPUSER_PORT_H_
 #define _LIB_LIBRUMPUSER_RUMPUSER_PORT_H_
 
-/*
- * Define things found by autoconf.  buildrump.sh defines RUMPUSER_CONFIG,
- * the NetBSD build does not run autoconf during build and supplies the
- * necessary values here.  To update the NetBSD values, run ./configure
- * for an up-to-date NetBSD installation and insert rumpuser_config.h
- * in the space below, e.g. with ":r !sed -ne '/^\#/p' rumpuser_config.h"
- */
-#if !defined(RUMPUSER_CONFIG)
-
 #define HAVE_ALIGNED_ALLOC 1
 #define HAVE_CLOCKID_T 1
 #define HAVE_CLOCK_GETTIME 1
@@ -28,32 +19,7 @@
 #define HAVE_UNISTD_H 1
 #define STDC_HEADERS 1
 
-#else /* RUMPUSER_CONFIG */
-#include "rumpuser_config.h"
-#endif
-
-#ifdef __linux__
-#define _GNU_SOURCE
-#endif
-
-#if defined(HAVE_SYS_CDEFS_H)
 #include <sys/cdefs.h>
-#endif
-
-/*
- * Some versions of FreeBSD (e.g. 9.2) contain C11 stuff without
- * any obvious way to expose the protos.  Kludge around it.
- */
-#ifdef __FreeBSD__
-#if __ISO_C_VISIBLE < 2011
-#undef __ISO_C_VISIBLE
-#define __ISO_C_VISIBLE 2011
-#endif
-#endif
-
-#if defined(HAVE_SYS_PARAM_H)
-#include <sys/param.h>
-#endif
 
 #ifndef MIN
 #define MIN(a,b)        ((/*CONSTCOND*/(a)<(b))?(a):(b))
