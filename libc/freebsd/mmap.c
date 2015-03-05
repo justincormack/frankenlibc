818c5838125096f9cf0dbb69d637dd6b1e6e7274
Justin Cormack <justin@specialbusservice.com>
2015-03-05 11:12:35 +0000

    fix handling of freebsd map aligned

--- libc/freebsd/mmap.c
+++ libc/freebsd/mmap.c
@@ -17,10 +17,13 @@ void *__mmap(void *, size_t, int, int, int, off_t);
 void *
 mmap(void *addr, size_t length, int prot, int nflags, int fd, off_t offset)
 {
-	int flags = nflags & (~FREEBSD_MAP_ALIGNED_SUPER) & (~MAP_STACK);
+	int flags = nflags & (~MAP_STACK);
 
 	if (nflags & MAP_STACK)
 		flags |= FREEBSD_MAP_STACK; 
+
+	if (nflags & MAP_ALIGNMENT_MASK == FREEBSD_MAP_ALIGNED_SUPER)
+		flags &= ~MAP_ALIGNMENT_MASK;
 
 	return __mmap(addr, length, prot, flags, fd, offset);
 }
