07550c982ace14afecab696b318d7f5dde0fa55f
Justin Cormack <justin@specialbusservice.com>
2015-03-18 13:38:36 +0000

    WIP arm port

---
+++ libc/linux/arm/linux.h
@@ -0,0 +1,25 @@
+#define TP_ADJ(p) (p)
+
+#define TCGETS 0x5401
+
+struct linux_stat {
+	unsigned long long	st_dev;
+	unsigned char		__pad0[4];
+	unsigned long		__st_ino;
+	unsigned int		st_mode;
+	unsigned int		st_nlink;
+	unsigned long		st_uid;
+	unsigned long		st_gid;
+	unsigned long long	st_rdev;
+	unsigned char		__pad3[4];
+	long long		st_size;
+	unsigned long		st_blksize;
+	unsigned long long	st_blocks;
+	unsigned long		st_atime;
+	unsigned long		st_atime_nsec;
+	unsigned long		st_mtime;
+	unsigned int		st_mtime_nsec;
+	unsigned long		st_ctime;
+	unsigned long		st_ctime_nsec;
+	unsigned long long	st_ino;
+};
