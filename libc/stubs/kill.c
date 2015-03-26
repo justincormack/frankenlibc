74eb18cb8584b470f2d8493648fa9044839dffe7
Justin Cormack <justin@specialbusservice.com>
2015-03-26 12:52:19 +0000

    fix kill name

--- libc/stubs/kill.c
+++ libc/stubs/kill.c
@@ -7,13 +7,13 @@
 #define SIGABRT 6
 
 int kill(pid_t, int);
-int __franken_kill(pid_t, int);
+int __platform_kill(pid_t, int);
 
 int
 kill(pid_t pid, int sig)
 {
 
-	__franken_kill(0, SIGABRT);
+	__platform_kill(0, SIGABRT);
 
 	return 0;
 }
