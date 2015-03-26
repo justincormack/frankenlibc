3df66266e7bc9eee6a1136152177a8f9d3a840d0
Justin Cormack <justin@specialbusservice.com>
2015-03-26 23:20:54 +0000

    remove debug print

--- libc/qemu-arm/__libc_start_main.c
+++ libc/qemu-arm/__libc_start_main.c
@@ -1,8 +1,6 @@
 #include <string.h>
 #include <stdint.h>
 
-ssize_t __platform_write(int fd, const void *buf, size_t count);
-
 extern int main(int, char **, char **);
 int argc = 1;
 char *argv[2] = {"qemu-arm", "\0"};
@@ -10,10 +8,9 @@ char *envp[2] = {"RUMP_VERBOSE=1", "\0"};
 
 size_t __hwcap = 407;
 
-int __libc_start_main() {
-	const char hello[] = "Hello bare metal arm world\n";
-
-	__platform_write(1, hello, sizeof(hello));
+int
+__libc_start_main()
+{
 
 	return __franken_start_main(main, argc, argv, envp);
 }
