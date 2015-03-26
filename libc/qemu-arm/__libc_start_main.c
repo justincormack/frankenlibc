9504f0085437b7abfec0e9d6a31313e30a8ddece
Justin Cormack <justin@specialbusservice.com>
2015-03-26 19:25:56 +0000

    use platform write for debug

--- libc/qemu-arm/__libc_start_main.c
+++ libc/qemu-arm/__libc_start_main.c
@@ -1,6 +1,7 @@
-#include <unistd.h>
 #include <string.h>
 #include <stdint.h>
+
+ssize_t __platform_write(int fd, const void *buf, size_t count);
 
 extern int main(int, char **, char **);
 int argc = 1;
@@ -12,7 +13,7 @@ size_t __hwcap = 407;
 int __libc_start_main() {
 	const char hello[] = "Hello bare metal arm world\n";
 
-	write(1, hello, strlen(hello));
+	__platform_write(1, hello, sizeof(hello));
 
 	return __franken_start_main(main, argc, argv, envp);
 }
