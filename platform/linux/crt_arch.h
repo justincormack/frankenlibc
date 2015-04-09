6f49feda2f382e49e5662d3e8df9823a6116a26f
Justin Cormack <justin@specialbusservice.com>
2015-04-09 21:08:16 +0000

    move to newer crt style

--- platform/linux/crt_arch.h
+++ platform/linux/crt_arch.h
@@ -1,17 +1,17 @@
 #if defined(__x86_64__)
-#include "x86_64/crt1.s"
+#include "x86_64/crt_arch.h"
 #elif defined(__i386__)
-#include "i386/crt1.s"
+#include "i386/crt_arch.h"
 #elif defined(__ARMEL__) || defined(__ARMEB__)
-#include "arm/crt1.s"
+#include "arm/crt_arch.h"
 #elif defined(__AARCH64EL__) || defined (__AARCH64EB__)
-#include "aarch64/crt1.s"
+#include "aarch64/crt_arch.h"
 #elif defined(__PPC64__)
-#include "powerpc64/crt1.s"
+#include "powerpc64/crt_arch.h"
 #elif defined(__PPC__)
-#include "powerpc/crt1.s"
+#include "powerpc/crt_arch.h"
 #elif defined(__MIPSEL__) || defined(__MIPSEB__)
-#include "mips/crt1.s"
+#include "mips/crt_arch.h"
 #else
 #error "Unknown architecture"
 #endif
