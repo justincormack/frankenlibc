26cfb68b40d2e9dbae914c7200b661d0190cf499
Justin Cormack <justin@specialbusservice.com>
2015-05-26 11:12:21 +0000

    more stubs needed for pthread_cancelstub

---
+++ franken/stubs/_sys.c
@@ -0,0 +1,11 @@
+#include "stub.h"
+
+NOTSUP(_sys_mq_send)
+NOTSUP(_sys_mq_receive)
+NOTSUP(_sys___mq_timedsend50)
+NOTSUP(_sys___mq_timedreceive50)
+NOTSUP(_sys_msgrcv)
+NOTSUP(_sys_msgsnd)
+NOTSUP(_sys___msync13)
+NOTSUP(_sys___wait450)
+NOTSUP(_sys___sigsuspend14)
