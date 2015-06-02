671a50b2737726e089ecf8d3ae6ca0acf33c6783
Justin Cormack <justin@specialbusservice.com>
2015-06-02 13:48:50 +0000

    fix qemu-arm for other compilers

--- platform/qemu-arm/platform.sh
+++ platform/qemu-arm/platform.sh
@@ -8,8 +8,8 @@ export OBJCOPY=${OBJCOPY-arm-none-eabi-objcopy}
 # arm-none-eabi-gcc does not turn this off when cpu set
 EXTRA_CPPFLAGS="-U__VFP_FP__"
 
-appendvar EXTRA_CFLAGS "-mcpu=arm926ej-s"
-appendvar EXTRA_AFLAGS "-mcpu=arm926ej-s"
+appendvar EXTRA_CFLAGS "-mcpu=arm926ej-s -marm -mfloat-abi=soft"
+appendvar EXTRA_AFLAGS "-mcpu=arm926ej-s -marm -D__SOFTFP__"
 
 # this compiler is very fussy, planning to fix these issues at some point
 appendvar EXTRA_CWARNFLAGS "-Wno-error"
